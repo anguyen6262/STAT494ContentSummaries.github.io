0998d1b579b30d5aa84ea2f7a933bfc30b958125
anguyen6262 <andrew.nguyen6262@gmail.com>
2022-09-15 17:00:42 -0500

    Added footer

---
+++ HTML CSS JavaScript/script.js
@@ -0,0 +1,16 @@
+
+const Summaries = document.querySelector(".summaries-menu");
+
+addEventListener('hover',() => Summaries.classList.toggle("active"))
+
+function contact() {
+    <div class="contact-section" id="author-contact-information">
+    <label > Author Contact Information</label>
+       <div class="social-icons">
+        <a href="https://github.com/anguyen6262"><i class="fab fa-github"></i></a>
+        <a href="https://www.linkedin.com/in/andrew-nguyen-6b72b0216/"><i class="fab fa-linkedin-in"></i></a>
+        <a href="https://www.facebook.com/profile.php?id=100079858971896"><i class="fab fa-facebook"></i></a>
+        <a href="https://www.instagram.com/andrew.d.ng/"><i class="fab fa-instagram"></i></a>
+    </div>
+    </div>
+}
