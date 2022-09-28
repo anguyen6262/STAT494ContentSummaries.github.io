3dd8b4fcb622b916ccaa6dd05514927471e3089c
anguyen6262 <andrew.nguyen6262@gmail.com>
2022-09-28 15:38:18 -0500

    working on images

--- HTML CSS JavaScript/script.js
+++ HTML CSS JavaScript/script.js
@@ -1,42 +1,18 @@
-// let toggleNavStatus = false;
+let getDropMenuBKButton = document.querySelector("#dropMenuBKButton");
+let getDropMenuCSButton = document.querySelector("#dropMenuCSButton");
+let getDropMenuBK = document.querySelector("#dropMenuBK");
+let getDropMenuCS = document.querySelector("#dropMenuCS");
 
-// let toggleNav = function() {
-//     let getContentDropMenu = document.querySelectorAll("#content-summaries.nav-dropMenu ul li:nth-child(n+2)");
-//     let getBKDropMenu = document.querySelectorAll("#background-knowledge.nav-dropMenu ul li:nth-child(n+2)");
-//     let getContentButton = document.getElementById('CsButton');
-// }
-  
-// let getContentDropMenu = document.querySelectorAll("#content-summaries.nav-dropMenu ul li:nth-child(n+2)");
-
-
-// function content(){
-    
-//         if(getContentDropMenu.style.visibility=="hidden") {
-//             for(let i = 0; i < getContentDropMenu.length; i++) {
-//                 getContentDropMenu[i].style.visibility = "visible";
-//             }
-
-//         } else {
-//             for(let j = 0; j < getContentDropMenu.length; j++) {
-//                 getContentDropMenu[j].style.visibility = "hidden";
-            
-//             }
-            
-//         }
-//     };
-        // getContentButton.style.color = "red";
-        // for(let i = 0; i < getContentDropMenu.length; i++) {
-        //             getContentDropMenu[i].style.visibility = "visible";
-        //             getContentDropMenu[i].style.opacity="0.8";
-    
-    // if(getContentButton:hover) {
-    //     for(let i = 0; i < getContentDropMenu.length; i++) {
-    //         getContentDropMenu[i].style.visibility = "visible";
-    //         getContentDropMenu[i].style.opacity="0.8";
-    //     }
-        // for(let i = 0; i < getBKDropMenu.length; i++) {
-        //     getBKDropMenu[i].style.visibility = "visible";
-        //     getBKDropMenu[i].style.opacity="0.8";
-        // }
-
-        // toggleNavStatus = true;
+function toggleDropMenu() {
+    getDropMenuBKButton.addEventListener("click", event => {
+        if(getDropMenuBK.style.visibility==hidden) {
+            getDropMenuBK.style.visibility=visible;
+        }
+    });
+    // if(getDropMenuBKButton.onmouseover) { 
+    //     getDropMenuBK.style.visibility = visible;
+    // }
+    // if(getDropMenuCSButton.onmouseover) {
+    //     getDropMenuCS.style.visibility = visible;
+    // }
+}
